07b87a40f83358d9437fe9a09ab8012db562e8f8
knightrishi <arnav.ara05@gmail.com>
2025-09-22 08:29:31 +0530

    Update LongestDivisiorInterval.java

--- LongestDivisiorInterval.java
+++ LongestDivisiorInterval.java
@@ -1,39 +1,24 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            long n = Long.parseLong(br.readLine());
-
-            Set<Long> divisors = new HashSet<>();
-            long limit = (long) Math.sqrt(n);
-            for (long i = 1; i <= limit; i++) {
-                if (n % i == 0) {
-                    divisors.add(i);
-                    divisors.add(n / i);
-                }
+import java.io.*;
+public class Main{
+	public static void main(String ar[]){
+		int t;
+	
+	
+		Scanner ob=new Scanner(System.in);
+		t=ob.nextInt();
+		
+		while(t-->0){
+    long n;
+		n=ob.nextLong();
+		List<Long> list=new ArrayList<>();
+		
+	 long i = 1;
+            while (n % i == 0) {
+                i++;
             }
-
-            List<Long> divisorList = new ArrayList<>(divisors);
-            Collections.sort(divisorList);
-
-            int count = 0;
-            for (int i = 1; i < divisorList.size(); i++) {
-                if (divisorList.get(i) - divisorList.get(i - 1) == 1) {
-                    count++;
-                }
-            }
-
-            out.println(count);
-        }
-
-        out.flush();
-        out.close();
-    }
-}
+ 
+		System.out.println(i-1);
+		
+ 
+		}
